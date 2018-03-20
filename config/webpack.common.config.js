7ab7672ebc30f0b74445d62ff47db46ee0f05063
Michael Roytman <mroytman@edx.org>
2018-03-20 14:18:25 -0400

    add webpack entry point

--- config/webpack.common.config.js
+++ config/webpack.common.config.js
@@ -6,6 +6,7 @@ module.exports = {
   entry: {
     assets: path.resolve(__dirname, '../src/index.jsx'),
     accessibilityPolicy: path.resolve(__dirname, '../src/accessibilityIndex.jsx'),
+    inContextImageSelection: path.resolve(__dirname, '../src/inContextImageSelection.jsx'),
     i18nMessages: path.resolve(__dirname, '../src/data/i18n/locales/currentlySupportedLangs.jsx'),
   },
   output: {
---
+++ src/editImageModalIndex.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable global-require,no-underscore-dangle */
+if ((typeof window !== 'undefined' && !window._babelPolyfill) ||
+  (typeof global !== 'undefined' && !global._babelPolyfill)) {
+  // Don't load babel-polyfill if already loaded: https://github.com/babel/babel/issues/4019
+  require('babel-polyfill'); // general ES2015 polyfill (e.g. promise)
+}
+/* eslint-enable global-require no-underscore-dangle */
+
+/* eslint-disable import/first */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import EditImageModal from './components/EditImageModal';
+/* eslint-enable import/first */
+
+const App = () => (
+  <div className="SFE-wrapper">
+    <EditImageModal />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
