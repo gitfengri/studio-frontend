8332ba267169c11144c50c37b490e6ce458bdb60
Michael Roytman <mroytman@edx.org>
2018-03-20 13:44:15 -0400

    sample modal

---
+++ src/components/EditImageModal/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Modal } from '@edx/paragon';
+
+const EditImageModal = () => (
+  <Modal
+    open
+    title="Edit Image"
+    body="Sample Edit Image Modal"
+    onClose={() => { }}
+  />
+);
+
+export default EditImageModal;
